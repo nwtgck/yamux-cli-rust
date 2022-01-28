4ff3fc50589439c11920a083f7fd26e4122f8341
Ryo Ota <nwtgck@nwtgck.org>
2022-01-28 23:52:10 +0900

    serve() works but bit strange

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod stdio;
 
 use anyhow::Result;
-use futures::{AsyncReadExt, TryStreamExt};
 
 #[tokio::main]
 async fn main() {
@@ -10,33 +9,67 @@ async fn main() {
 }
 
 async fn serve() -> Result<()> {
-    let stdio = stdio::Stdio {};
-    let yamux_config = yamux::Config::default();
+    use futures::{AsyncReadExt, TryStreamExt};
+
+    let stdio = stdio::Stdio::new();
+    let mut yamux_config = yamux::Config::default();
+    // yamux_config.set_max_buffer_size(23);
+    // yamux_config.set_read_after_close(true);
     let yamux_server = yamux::Connection::new(stdio, yamux_config, yamux::Mode::Server);
     yamux::into_stream(yamux_server)
         .try_for_each_concurrent(None, |yamux_stream| async move {
-            let (yamux_stream_read, mut yamux_write) = yamux_stream.split();
-            let (connection_read, mut connection_write) = {
-                use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
-
-                // TODO: hard code
-                let (r, w) = tokio::net::TcpStream::connect("127.0.0.1:2022")
+            eprintln!("new yamux_stream");
+            use tokio_util::compat::{FuturesAsyncReadCompatExt, FuturesAsyncWriteCompatExt};
+            let (yamux_stream_read, yamux_write) = yamux_stream.split();
+            let (mut connection_read, mut connection_write) =
+                tokio::net::TcpStream::connect("127.0.0.1:2022")
                     .await?
                     .into_split();
-                (r.compat(), w.compat_write())
-            };
-            tokio::task::spawn(async move {
-                futures::io::copy(yamux_stream_read, &mut connection_write)
-                    .await
-                    .unwrap();
+
+            tokio::spawn(async move {
+                let mut buf = [0u8; 16];
+                copy_with_buffer(
+                    &mut yamux_stream_read.compat(),
+                    &mut connection_write,
+                    &mut buf,
+                )
+                .await
+                .unwrap();
             });
-            tokio::task::spawn(async move {
-                futures::io::copy(connection_read, &mut yamux_write)
-                    .await
-                    .unwrap();
+            tokio::spawn(async move {
+                let mut buf = [0u8; 16];
+                copy_with_buffer(
+                    &mut connection_read,
+                    &mut yamux_write.compat_write(),
+                    &mut buf,
+                )
+                .await
+                .unwrap();
+                // tokio::io::copy(&mut connection_read, &mut yamux_write.compat_write())
+                //     .await
+                //     .unwrap();
             });
             Ok(())
         })
         .await?;
+    Ok(())
+}
+
+async fn copy_with_buffer<R: tokio::io::AsyncRead + Unpin, W: tokio::io::AsyncWrite + Unpin>(
+    reader: &mut R,
+    writer: &mut W,
+    buf: &mut [u8],
+) -> tokio::io::Result<()> {
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+    eprintln!("copy with buffer start");
+    loop {
+        let n = reader.read(buf).await?;
+        eprintln!("read {:?} in copy", n);
+        if n <= 0 {
+            break;
+        }
+        writer.write_all(&buf[..n]).await?;
+        writer.flush().await?;
+    }
     Ok(())
 }
--- src/stdio.rs
+++ src/stdio.rs
@@ -1,7 +1,26 @@
+use pin_project_lite::pin_project;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use tokio::io::AsyncWrite;
 
-pub struct Stdio;
+pin_project! {
+    pub struct Stdio {
+        #[pin]
+        stdin: tokio_util::compat::Compat<tokio::io::Stdin>,
+        #[pin]
+        stdout: tokio_util::compat::Compat<tokio::io::Stdout>,
+    }
+}
+
+impl Stdio {
+    pub fn new() -> Self {
+        use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
+        Stdio {
+            stdin: tokio::io::stdin().compat(),
+            stdout: tokio::io::stdout().compat_write(),
+        }
+    }
+}
 
 impl futures::AsyncRead for Stdio {
     fn poll_read(
@@ -9,10 +28,17 @@ impl futures::AsyncRead for Stdio {
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<std::io::Result<usize>> {
+        use tokio_util::compat::TokioAsyncReadCompatExt;
+        let mut this = self.project();
         eprintln!("> poll read: {:?}", buf);
-        let res = Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdin())).poll_read(cx, buf);
+        // let res = Pin::new(&mut tokio::io::stdin().compat()).poll_read(cx, buf);
+        let res = this.stdin.poll_read(cx, buf);
         eprintln!("< poll read: {:?}", buf);
         return res;
+        // eprintln!("> poll read: {:?}", buf);
+        // let res = Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdin())).poll_read(cx, buf);
+        // eprintln!("< poll read: {:?}", buf);
+        // return res;
     }
 }
 
@@ -22,18 +48,30 @@ impl futures::AsyncWrite for Stdio {
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
+        use tokio_util::compat::TokioAsyncWriteCompatExt;
+        let mut this = self.project();
+
         eprintln!("> poll write: {:?}", buf);
-        let res =
-            Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdout())).poll_write(cx, buf);
+        // let res =
+        //     Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdout())).poll_write(cx, buf);
+        // std::io::stdout().flush();
+        // let res = Pin::new(&mut tokio::io::stdout().compat_write()).poll_write(cx, buf);
+        let res = this.stdout.poll_write(cx, buf);
         eprintln!("< poll write: {:?}", buf);
         return res;
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
-        return Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdout())).poll_flush(cx);
+        use tokio_util::compat::TokioAsyncWriteCompatExt;
+        let mut this = self.project();
+        // return Pin::new(&mut tokio::io::stdout().compat_write()).poll_flush(cx);
+        return this.stdout.poll_flush(cx);
     }
 
     fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
-        return Pin::new(&mut futures::io::AllowStdIo::new(std::io::stdout())).poll_close(cx);
+        use tokio_util::compat::TokioAsyncWriteCompatExt;
+        let mut this = self.project();
+        // return Pin::new(&mut tokio::io::stdout().compat_write()).poll_flush(cx);
+        return this.stdout.poll_close(cx);
     }
 }
