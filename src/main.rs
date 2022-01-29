0c29d5235cc5d76c12793ef1ad7f96f72db5ecee
Ryo Ota <nwtgck@nwtgck.org>
2022-01-29 12:04:33 +0900

    implement yamux client

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,7 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     if args.listen {
-        todo!();
-        return Err(anyhow::Error::msg("listen not implemented yet"));
+        return run_yamux_client().await;
     }
 
     if args.rest_args.len() != 2 {
@@ -37,16 +36,16 @@ async fn main() -> Result<()> {
     let host: &str = &args.rest_args[0];
     let port: u16 = args.rest_args[1].parse()?;
 
-    serve(host, port).await.unwrap();
+    run_yamux_server(host, port).await.unwrap();
     Ok(())
 }
 
-async fn serve(host: &str, port: u16) -> Result<()> {
+async fn run_yamux_server(host: &str, port: u16) -> Result<()> {
     use futures::{AsyncReadExt, TryStreamExt};
 
-    let stdio = stdio::Stdio::new();
     let yamux_config = yamux::Config::default();
-    let yamux_server = yamux::Connection::new(stdio, yamux_config, yamux::Mode::Server);
+    let yamux_server =
+        yamux::Connection::new(stdio::Stdio::new(), yamux_config, yamux::Mode::Server);
     yamux::into_stream(yamux_server)
         .try_for_each_concurrent(None, |yamux_stream| async move {
             let (yamux_stream_read, yamux_write) = yamux_stream.split();
@@ -54,21 +53,69 @@ async fn serve(host: &str, port: u16) -> Result<()> {
                 tokio::net::TcpStream::connect((host, port))
                     .await?
                     .into_split();
-            let f1 = async move {
+            let fut1 = async move {
                 use tokio_util::compat::FuturesAsyncReadCompatExt;
                 tokio::io::copy(&mut yamux_stream_read.compat(), &mut connection_write)
                     .await
                     .unwrap();
             };
-            let f2 = async move {
+            let fut2 = async move {
                 use tokio_util::compat::FuturesAsyncWriteCompatExt;
                 tokio::io::copy(&mut connection_read, &mut yamux_write.compat_write())
                     .await
                     .unwrap();
             };
-            futures::future::join(f1, f2).await;
+            futures::future::join(fut1, fut2).await;
             Ok(())
         })
         .await?;
     Ok(())
+}
+
+async fn run_yamux_client() -> Result<()> {
+    use futures::AsyncReadExt;
+
+    let yamux_config = yamux::Config::default();
+    let yamux_client =
+        yamux::Connection::new(stdio::Stdio::new(), yamux_config, yamux::Mode::Client);
+    let yamux_control = yamux_client.control();
+
+    let yamux_stream = yamux::into_stream(yamux_client);
+    tokio::task::spawn({
+        use futures::StreamExt;
+        yamux_stream.for_each(|_| async {})
+    });
+
+    // TODO: hard code
+    let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:3322").await?;
+
+    loop {
+        let (socket, _) = tcp_listener.accept().await?;
+        let (mut socket_read, mut socket_write) = socket.into_split();
+
+        let mut yamux_control = yamux_control.clone();
+        tokio::task::spawn(async move {
+            let yamux_stream_result = yamux_control.open_stream().await;
+            if let Err(err) = yamux_stream_result {
+                // TODO: logging
+                eprintln!("failed to open: {:?}", err);
+                return;
+            }
+            let (yamux_stream_read, yamux_stream_write) = yamux_stream_result.unwrap().split();
+
+            let fut1 = async move {
+                use tokio_util::compat::FuturesAsyncReadCompatExt;
+                tokio::io::copy(&mut yamux_stream_read.compat(), &mut socket_write)
+                    .await
+                    .unwrap();
+            };
+            let fut2 = async move {
+                use tokio_util::compat::FuturesAsyncWriteCompatExt;
+                tokio::io::copy(&mut socket_read, &mut yamux_stream_write.compat_write())
+                    .await
+                    .unwrap();
+            };
+            futures::future::join(fut1, fut2).await;
+        });
+    }
 }
